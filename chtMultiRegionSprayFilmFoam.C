afbb236da63f5341f0a143075757de0bbcc9ef0b
Subhkaran Singh <69869942+barbariansubhkaran@users.noreply.github.com>
2022-06-02 05:38:38 +0300

    Add files via upload

---
+++ chtMultiRegionSprayFilmFoam.C
@@ -0,0 +1,64 @@
+#include "fvCFD.h"
+#include "turbulenceModel.H"
+
+#include "basicSprayCloud.H"
+#include "surfaceFilmModel.H"
+
+#include "SLGThermo.H"
+#include "psiChemistryCombustionModel.H"
+
+#include "fixedGradientFvPatchFields.H"
+#include "regionProperties.H"
+#include "compressibleCourantNo.H"
+#include "solidRegionDiffNo.H"
+#include "basicSolidThermo.H"
+
+
+int main(int argc, char *argv[])
+{
+     #include "setRootCase.H"
+     #include "createTime.H"
+
+     regionProperties rp(runTime);
+
+     #include "createFluidMeshes.H"
+     #include "createSolidMeshes.H"
+
+     #include "createFluidFields.H"
+     #include "createSolidFields.H"
+
+     forAll(fluidRegions, i)
+     {
+
+                 Info << "\n___________ Adding to tsurfaceFilm for " << fluidRegions[i].name() << "__________\n" << endl;
+
+                typedef regionModels::surfaceFilmModels::surfaceFilmModel filmModelType;
+
+                tsurfaceFilm.set
+                (
+                  i,
+                  filmModelType::New(fluidRegions[i], gFluid[i])
+                );
+    }
+
+    #include "initContinuityErrs.H"
+    #include "readTimeControls.H"
+    #include "readSolidTimeControls.H"
+    #include "compressibleMultiRegionCourantNo.H"
+    #include "solidRegionDiffusionNo.H"
+    #include "setInitialMultiRegionDeltaT.H"
+
+    while(runTime.run())
+    {
+          Info << "NEW TIMESTEP" << endl;
+
+          
+    }
+
+
+
+
+
+
+
+}
